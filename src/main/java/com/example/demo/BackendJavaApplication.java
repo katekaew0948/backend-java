31f09d69d9d8c9ec654ccb879cfa6471c79ae29a
katekaew0948 <6410210036@psu.ac.th>
2023-07-17 17:02:46 +0700

    lab2 getall,create Employee

--- src/main/java/com/example/demo/BackendJavaApplication.java
+++ src/main/java/com/example/demo/BackendJavaApplication.java
@@ -8,7 +8,8 @@ public class BackendJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendJavaApplication.class, args);
-		System.out.println("katekaew");
+		System.out.println("Hello spring");
+		System.out.println("Spring boot");
 	}
 
 }
