56acebd3576cec2cdc70a43c75af5081205d5ffd
katekaew0948 <6410210036@psu.ac.th>
2023-07-19 12:04:40 +0700

    Lab2 put,delete,CRUD excercise

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -3,8 +3,11 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +35,51 @@ public class EmployeeController {
 		
 		data.add(body);
 		return body;
+	}
+	
+	@GetMapping("/employee/{employeeId}")
+	public Employee getEmployeeDetail(@PathVariable Integer employeeId) {
+		System.out.println(employeeId);
+		
+		for(int i = 0; i < data.size();i++) {
+			if(data.get(i).getEmployeeId() == employeeId) {
+				return data.get(i);
+			}
+		}
+		
+		return null;
+	}
+	
+	@PutMapping("/employee/{employeeId}")
+	public Employee updateEmployee(@PathVariable Integer employeeId ,@RequestBody Employee body) {
+		
+		for(int i = 0; i < data.size();i++) {
+			if(data.get(i).getEmployeeId() == employeeId) {
+				
+				data.get(i).setFirstName(body.getFirstName());
+				data.get(i).setLastName(body.getLastName());
+				data.get(i).setSalary(body.getSalary());
+				
+				return data.get(i);
+			}
+		}
+		
+		return null;
+	}
+	
+	@DeleteMapping("/employee/{employeeId}")
+	public String deleteEmployee(@PathVariable Integer employeeId) {
+		
+		for(int i = 0; i < data.size();i++) {
+			if(data.get(i).getEmployeeId() == employeeId) {
+				
+				data.remove(i);
+				
+				return "delete success";
+			}
+		}
+		
+		return "employee not found";
 	}
 	
 }
---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,86 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Employee;
+import com.example.demo.model.Product;
+
+@RestController
+public class ProductController {
+	
+	private List<Product> data = new ArrayList<Product>();
+	
+	@GetMapping("product")
+	public List<Product> getProduct() {
+		
+		return data;
+	}
+	
+	@GetMapping("/product/{productId}")
+	public Product getProductDetail(@PathVariable Integer productId) {
+		
+		for(int i = 0; i < data.size();i++) {
+			if(data.get(i).getProductId() == productId) {
+				
+				return data.get(i);
+			}
+		}
+		return null;
+	}
+	
+	@PostMapping("/product")
+	public Product addProduct(@RequestBody Product body) {
+		
+		for(int i = 0; i < data.size();i++) {
+			if(data.get(i).getProductId() == body.getProductId()) {
+				
+				return null;
+			}
+		}
+		
+		data.add(body);
+		return body;
+	}
+	
+	@PutMapping("/product/{productId}")
+	public Product updateProduct(@PathVariable Integer productId ,@RequestBody Product body) {
+		
+		for(int i = 0; i < data.size();i++) {
+			if(data.get(i).getProductId() == productId) {
+				
+				data.get(i).setProductName(body.getProductName());
+				data.get(i).setProductPrice(body.getProductPrice());
+				data.get(i).setProductDetail(body.getProductDetail());
+				data.get(i).setProductAmount(body.getProductAmount());
+				return data.get(i);
+			}
+		}
+		
+		return null;
+	}
+	
+	@DeleteMapping("/product/{productId}")
+	public String deleteProduct(@PathVariable Integer productId) {
+		
+		for(int i = 0; i < data.size();i++) {
+			if(data.get(i).getProductId() == productId) {
+				
+				data.remove(i);
+				
+				return "Delete success";
+			}
+		}
+		
+		return "Product not found";
+	}
+
+}
