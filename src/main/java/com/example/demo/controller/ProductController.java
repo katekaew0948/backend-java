c83787ddb75b482a7c8ecd0bd8dc5ccaf0d0ef40
katekaew0948 <6410210036@psu.ac.th>
2023-07-19 12:46:37 +0700

    Lab2 text_search

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Employee;
@@ -16,47 +17,48 @@ import com.example.demo.model.Product;
 
 @RestController
 public class ProductController {
-	
+
 	private List<Product> data = new ArrayList<Product>();
-	
+	private List<Product> dataSearch = new ArrayList<Product>();
+
 	@GetMapping("product")
 	public List<Product> getProduct() {
-		
+
 		return data;
 	}
-	
+
 	@GetMapping("/product/{productId}")
 	public Product getProductDetail(@PathVariable Integer productId) {
-		
-		for(int i = 0; i < data.size();i++) {
-			if(data.get(i).getProductId() == productId) {
-				
+
+		for (int i = 0; i < data.size(); i++) {
+			if (data.get(i).getProductId() == productId) {
+
 				return data.get(i);
 			}
 		}
 		return null;
 	}
-	
+
 	@PostMapping("/product")
 	public Product addProduct(@RequestBody Product body) {
-		
-		for(int i = 0; i < data.size();i++) {
-			if(data.get(i).getProductId() == body.getProductId()) {
-				
+
+		for (int i = 0; i < data.size(); i++) {
+			if (data.get(i).getProductId() == body.getProductId()) {
+
 				return null;
 			}
 		}
-		
+
 		data.add(body);
 		return body;
 	}
-	
+
 	@PutMapping("/product/{productId}")
-	public Product updateProduct(@PathVariable Integer productId ,@RequestBody Product body) {
-		
-		for(int i = 0; i < data.size();i++) {
-			if(data.get(i).getProductId() == productId) {
-				
+	public Product updateProduct(@PathVariable Integer productId, @RequestBody Product body) {
+
+		for (int i = 0; i < data.size(); i++) {
+			if (data.get(i).getProductId() == productId) {
+
 				data.get(i).setProductName(body.getProductName());
 				data.get(i).setProductPrice(body.getProductPrice());
 				data.get(i).setProductDetail(body.getProductDetail());
@@ -64,23 +66,36 @@ public class ProductController {
 				return data.get(i);
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	@DeleteMapping("/product/{productId}")
 	public String deleteProduct(@PathVariable Integer productId) {
-		
-		for(int i = 0; i < data.size();i++) {
-			if(data.get(i).getProductId() == productId) {
-				
+
+		for (int i = 0; i < data.size(); i++) {
+			if (data.get(i).getProductId() == productId) {
+
 				data.remove(i);
-				
+
 				return "Delete success";
 			}
 		}
-		
+
 		return "Product not found";
+	}
+
+	@GetMapping("/product/search_text")
+	public List<Product> searchProducts(@RequestParam String name) {
+
+		for (int i = 0; i < data.size(); i++) {
+			if (data.get(i).getProductName().contains(name) || data.get(i).getProductDetail().contains(name)) {
+
+				dataSearch.add(data.get(i));
+			}
+
+		}
+		return dataSearch;
 	}
 
 }
