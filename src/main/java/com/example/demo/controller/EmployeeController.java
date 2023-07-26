adc5ef893d92b3853db5d94fe77e11a8dfcac3c7
katekaew0948 <6410210036@psu.ac.th>
2023-07-26 12:22:43 +0700

    lab3

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,74 +14,70 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
 
 @RestController
 public class EmployeeController {
-
-	private List<Employee> data = new ArrayList<Employee>();
 	
-	@GetMapping("employee")
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	
+	@GetMapping("/employee")
 	public List<Employee> getEmployee() {
 		
-		return data;
+		return employeeRepository.findAll();
 	}
 	
 	@PostMapping("/employee")
 	public Employee addEmployee(@RequestBody Employee body) {
 		
-		for(int i = 0; i < data.size();i++) {
-			if(data.get(i).getEmployeeId() == body.getEmployeeId()) {
-				return null;
-			}
-		}
-		
-		data.add(body);
-		return body;
+		return employeeRepository.save(body);
 	}
 	
 	@GetMapping("/employee/{employeeId}")
-	public Employee getEmployeeDetail(@PathVariable Integer employeeId) {
-		System.out.println(employeeId);
+	public Optional<Employee> getEmployeeDetail(@PathVariable Integer employeeId) {
+	
+		Optional<Employee> employee = employeeRepository.findById(employeeId);
 		
-		for(int i = 0; i < data.size();i++) {
-			if(data.get(i).getEmployeeId() == employeeId) {
-				return data.get(i);
-			}
-		}
-		
-		return null;
+		return employee;
 	}
 	
 	@PutMapping("/employee/{employeeId}")
 	public Employee updateEmployee(@PathVariable Integer employeeId ,@RequestBody Employee body) {
 		
-		for(int i = 0; i < data.size();i++) {
-			if(data.get(i).getEmployeeId() == employeeId) {
-				
-				data.get(i).setFirstName(body.getFirstName());
-				data.get(i).setLastName(body.getLastName());
-				data.get(i).setSalary(body.getSalary());
-				
-				return data.get(i);
-			}
+		Optional<Employee> employee = employeeRepository.findById(employeeId);
+		
+		if(employee.isPresent()) {
+			employee.get().setFirstName(body.getFirstName());
+			employee.get().setLastName(body.getLastName());
+			employee.get().setSalary(body.getSalary());
+			employee.get().setEmployeeId(body.getEmployeeId());
+			
+			employeeRepository.save(employee.get());
+			
+			return employee.get() ;
+		}
+		else {
+			return null;
 		}
 		
-		return null;
 	}
 	
 	@DeleteMapping("/employee/{employeeId}")
 	public String deleteEmployee(@PathVariable Integer employeeId) {
 		
-		for(int i = 0; i < data.size();i++) {
-			if(data.get(i).getEmployeeId() == employeeId) {
-				
-				data.remove(i);
-				
-				return "delete success";
-			}
+		Optional<Employee> employee = employeeRepository.findById(employeeId);
+		
+		if(employee.isPresent()) {
+			employeeRepository.delete(employee.get());
+			
+			return "DELETE SUCSESS";
+		}
+		else {
+			return "Employee NOT FOUND";
 		}
 		
-		return "employee not found";
 	}
 	
 }
--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,9 @@ import com.example.demo.model.Product;
 @RestController
 public class ProductController {
 
+	@Autowired
+	
+	
 	private List<Product> data = new ArrayList<Product>();
 	private List<Product> dataSearch = new ArrayList<Product>();
 
