9b794b4ddd82be4e7a462459744d2bd75a4d8e7e
katekaew0948 <6410210036@psu.ac.th>
2023-08-31 14:55:40 +0700

    midterm

---
+++ src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,58 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String studentID;
+	private String firstName;
+	private String lastName;
+	private String email;
+	public Student(String studentID, String firstName, String lastName, String email) {
+		super();
+		this.studentID = studentID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getStudentID() {
+		return studentID;
+	}
+	public void setStudentID(String studentID) {
+		this.studentID = studentID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
